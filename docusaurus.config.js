f1ba975bc27b5bd06f440e0cfc8eb9873e543967
devilkiller-ag <ashmitgupta.social@gmail.com>
2023-12-04 22:41:36 +0530

    updated github workflow for pr preview

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   url: 'https://uptane.org',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: process.env.PULL_REQUEST ? `/pr-${process.env.PULL_REQUEST_NUMBER}/` : '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.

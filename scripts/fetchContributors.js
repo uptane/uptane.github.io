88b525d9db59b0bd1489e978e47a388e1eb0577e
Ashmit JaiSarita Gupta <43639341+devilkiller-ag@users.noreply.github.com>
2023-12-14 08:21:43 +0000

    🛠️  Added gh workflow for fetching contributors every midnight

--- scripts/fetchContributors.js
+++ scripts/fetchContributors.js
@@ -1,15 +1,25 @@
 const axios = require('axios');
 const fs = require('fs');
 
+const accessToken = process.env.REACT_APP_GITHUB_ACCESS_TOKEN;
+
 const getRepositories = async () => {
     const url = `https://api.github.com/orgs/uptane/repos`;
-    const response = await axios.get(url);
+    const response = await axios.get(url, {
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+    });
     return response.data;
 }
 
 const getContributors = async (repoName) => {
     const url = `https://api.github.com/repos/uptane/${repoName}/contributors`;
-    const response = await axios.get(url);
+    const response = await axios.get(url, {
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+    });
     return response.data;
 }
 
@@ -25,7 +35,7 @@ const fetchContributors = async () => {
         }
 
         const jsonContent = JSON.stringify(contributorsList, null, 2);
-        fs.writeFileSync('./../contributors.json', jsonContent);
+        fs.writeFileSync('./contributors.json', jsonContent);
         console.log('SUCCESS :: scripts/fetchContributors.js :: Contributors list saved to contributors.json');
     } catch (error) {
         console.error('ERROR :: scripts/fetchContributors.js :: ', error.message);

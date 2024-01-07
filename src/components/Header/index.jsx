8c4a2de33bb2104b5b5337a1392445a8aac65c97
Abhijay007 <Abhijay007j@gmail.com>
2024-01-07 23:01:03 +0530

    (Fix): Updated Hero Section Uptane Logo
    Signed-off-by: Abhijay007 <Abhijay007j@gmail.com>

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import UptaneCubes from '@site/static/img/uptane_hero_img.svg';
 import styles from './styles.module.css';
+import ThemedImage from '@theme/ThemedImage';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 import Link from '@docusaurus/Link';
 
 export default function Header() {
@@ -10,7 +12,14 @@ export default function Header() {
     return (
         <div className={headerBackClasses}>
             <div className={styles.header_content}>
-                <h1>UPTANE</h1>
+            <ThemedImage
+                alt="Supported By Logo"
+                width="300px"
+                sources={{
+                    light: useBaseUrl('/img/uptaneLogoHero.svg'),
+                    dark: useBaseUrl('/img/uptaneLogoHero.svg'),
+                }}
+            />
                 <h3>A Secure Software Update Framework for Automobiles</h3>
                 <div className={styles.header_content_buttons}>
                     <Link to="/learn-more/getting-started" className={customButtonClasses}>Learn More</Link>
